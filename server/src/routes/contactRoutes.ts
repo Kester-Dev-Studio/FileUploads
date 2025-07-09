7e7d02bfb64f99f5c4a238b1f8a7744d3a1eb9a2
Opia12345 <preciousopia7@gmail.com>
2025-07-09 17:40:43 +0100

    feat: included the production build

--- server/src/routes/contactRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { sendContactMessage } from "../controllers/contactController";
-
-const router = Router();
-
-router.post("/contact", sendContactMessage);
-
-export default router;
--- server/src/routes/emailRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { sendTestEmail } from "../controllers/emailController";
-
-const router = Router();
-
-router.post("/send-test-email", sendTestEmail);
-
-export default router;
