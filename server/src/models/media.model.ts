8bd8c9751ad47b464b9712dc51335c9ca2b77baf
Opia12345 <preciousopia7@gmail.com>
2025-07-09 18:04:59 +0100

    feat: added the frontend handler for new select fields

--- server/src/models/media.model.ts
+++ server/src/models/media.model.ts
@@ -4,7 +4,13 @@ export interface IMedia extends Document {
   title: string;
   description: string;
   tags: string[];
-  category: string;
+  category:
+    | "UI/UX"
+    | "Blockchain Development"
+    | "Web Development"
+    | "2D/3D design"
+    | "Game Development"
+    | "App Development";
   images: string[];
   problem: string;
   solution: string;
@@ -15,7 +21,18 @@ const mediaSchema = new Schema<IMedia>(
     title: { type: String, required: true },
     description: { type: String, required: true },
     tags: { type: [String], required: true },
-    category: { type: String, required: true },
+    category: {
+      type: String,
+      required: true,
+      enum: [
+        "UI/UX",
+        "Blockchain Development",
+        "Web Development",
+        "2D/3D design",
+        "Game Development",
+        "App Development",
+      ],
+    },
     images: { type: [String], required: true },
     problem: { type: String, required: true },
     solution: { type: String, required: true },
