21e69549f9263ea26866ddac9f9fd662fbf4b870
Opia12345 <preciousopia7@gmail.com>
2025-07-12 20:11:17 +0100

    feat: completed the management dashbaord

--- server/src/routes/upload.routes.ts
+++ server/src/routes/upload.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import multer from "multer";
-import { uploadMedia, getSingleUpload } from "../controllers/uploadController";
+import { uploadMedia, getSingleUpload, editUpload, deleteUpload } from "../controllers/uploadController";
 import { allMedia } from "../controllers/mediaController";
 
 const router = Router();
@@ -9,5 +9,7 @@ const upload = multer({ dest: "uploads/" });
 router.post("/upload", upload.array("images"), uploadMedia);
 router.get("/uploads/:id", getSingleUpload);
 router.get("/uploads", allMedia);
+router.put("/uploads/:id", upload.array("newImages"), editUpload);
+router.delete("/uploads/:id", deleteUpload);
 
 export default router;
